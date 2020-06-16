480d0d0bc1a00e0e6524f68d1629305108640db6
KristianFJones <me@kristianjones.dev>
2020-06-16 15:08:04 +0000

    chore(dev): Upgrade Development

--- src/Web/Client.tsx
+++ src/Web/Client.tsx
@@ -9,8 +9,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { useWebSockets } from './Hooks/useWebsockets';
 import { registerExportsForReactRefresh } from './Library/Helper';
 
+/**
+ * Count needed so we can request an import with a new param each HMR
+ */
 let count = 0;
 
+/**
+ * Render the Client Side
+ */
 async function renderClient(): Promise<void> {
   const container = document.getElementById('app')!;
 
