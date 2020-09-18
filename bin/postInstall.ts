385de0cd9065a49ccc6f0438762ac17edee6e3ca
KristianFJones <me@kristianjones.dev>
2020-09-18 22:34:02 +0000

    chore(deps): Get everything working  after update!

--- bin/postInstall.ts
+++ bin/postInstall.ts
@@ -1,16 +1,18 @@
 // bin/postInstall.ts
-// import { processModule } from './Utils/proceseNodeModule';
+import { processModule } from './Utils/proceseNodeModule';
 import { cpFile } from './Utils/cpFile';
 
-/*
 const cjsFiles: string[] = [
   'react-dom/cjs/react-dom.development.js',
   'react-dom/cjs/react-dom.production.min.js',
   'react-refresh/cjs/react-refresh-runtime.development.js',
   'react-is/cjs/react-is.development.js',
+  'prop-types/node_modules/react-is/cjs/react-is.development.js',
 ];
 
-console.log(`Processing module files: `, cjsFiles); */
+console.log(`Processing module files: `, cjsFiles);
+
+await Promise.all(cjsFiles.map(processModule));
 
 await Promise.all([
   cpFile('extras/history/package.json', 'node_modules/history/package.json'),
